3591425f49b10bd8b7ab813c8c160d4b7d61914f
Makemek <makemek101@hotmail.com>
2015-05-19 18:49:31 +0700

    use logcat and refactor

--- logrecorder/src/main/java/com/example/make/logrecorder/DBOperation.java
+++ logrecorder/src/main/java/com/example/make/logrecorder/DBOperation.java
@@ -3,6 +3,7 @@ package com.example.make.logrecorder;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.example.make.logrecorder.table.DatabaseInfo;
 import com.example.make.logrecorder.table.TestResult;
@@ -13,24 +14,27 @@ import com.example.make.logrecorder.table.TestResult;
 public class DBOperation extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1; // schema version
-
+    public static final String LOGCAT_DB = "Database";
     public DBOperation(Context context) {
         super(context, DatabaseInfo.DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(LOGCAT_DB, "database created");
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String create_query = "CREATE TABLE " + DatabaseInfo.TABLE_NAME;
-        String not_null = "NOT NULL";
         String columnData = "(" +
-                TestResult.id + " INT PRIMARY KEY AUTOINCREMENT, " + not_null +
-                TestResult.level + " INT CHECK" + "(" + TestResult.level + " BETWEEN 1 AND 5" + ")" +
-                TestResult.duration + " INT, " + not_null +
-                TestResult.balance + " INT, " + not_null +
-                TestResult.totalBalance + " INT " + not_null
+                TestResult.id + " INT PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                TestResult.level + " INT CHECK" + "(" + TestResult.level + " BETWEEN 1 AND 5" + "), " +
+                TestResult.duration + " INT NOT NULL," +
+                TestResult.balance + " INT, NOT NULL, " +
+                TestResult.totalBalance + " INT NOT NULL"
                 ;
 
         create_query += columnData;
+
+        sqLiteDatabase.execSQL(create_query);
+        Log.d(LOGCAT_DB, DatabaseInfo.TABLE_NAME + " is created");
     }
 
     @Override
