c661a27c83b99518578a3f12be1a1293abad1349
Pongrawee <wpj_4@hotmail.com>
2015-05-20 00:23:29 +0700

    initial balanceDetector commit

--- app/src/main/java/com/starboy/karav/SA/Process.java
+++ app/src/main/java/com/starboy/karav/SA/Process.java
@@ -1,21 +1,33 @@
 package com.starboy.karav.SA;
 
 public class Process {
-	private float xbase;
-	private float ybase;
 	private int level;
+	private double x_max =0, x_min=0;
+	private double y_max =0, y_min =0;
+	private boolean x_status = false;
+	private boolean y_status = false;
 
 	public void setlevel(int l) {
-		level = l;
+		int num = 0;
+		while(num == l) {
+			x_max = x_max+0.1;
+			x_min = x_min-0.1;
+			y_max = y_min+0.1;
+			y_min = y_min-0.1;
+			num++;
+		}
 	}
-
-	public void setbase(float x, float y) {
-		xbase = x;
-		ybase = y;
-	}
-
-	public boolean process(float x, float y) {
-		return true;
+	public boolean process(double x, double y) {
+		if(x>=x_min && x<=x_max){
+			x_status = true;
+		}
+		if(y>=y_min && y<=y_max){
+			y_status = true;
+		}
+		if(x_status == true && y_status == true){
+			return true;
+		}
+		return false;
 	}
 
 }
