b232428d0c25914769351d642917043b5122d9d6
Karunyapas Dangruan <ioscarhunter@gmail.com>
2015-05-19 19:35:37 +0700

    add bluetooth Activity\ for bluetooth connect

--- app/src/main/java/com/starboy/karav/SA/ReceiverActivity.java
+++ app/src/main/java/com/starboy/karav/SA/ReceiverActivity.java
@@ -3,106 +3,22 @@ package com.starboy.karav.SA;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class ReceiverActivity extends ColourBarActivity {
+public class ReceiverActivity extends BluetoothActivity implements RecieverFragmentListener {
 	private static final int REQUEST_ENABLE_BT = 3;
 	private static final int REQUEST_GET_DEVICE = 2;
 	public static String EXTRA_DEVICE_ADDRESS = "device_address";
 	private String TAG = "ReceiverActivity";
 
-	/**
-	 * Name of the connected device
-	 */
-	private String mConnectedDeviceName = null;
-	/**
-	 * The Handler that gets information back from the BluetoothChatService
-	 */
-	private final Handler mHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			Activity activity = getActivity();
-			switch (msg.what) {
-				case Constants.MESSAGE_STATE_CHANGE:
-					switch (msg.arg1) {
-						case BluetoothChatService.STATE_CONNECTED:
-//                            (getActivity()).setTitle(getString(R.string.title_connected_to, mConnectedDeviceName));
-//                            mConversationArrayAdapter.clear();
-							break;
-						case BluetoothChatService.STATE_CONNECTING:
-//                            setStatus(R.string.title_connecting);
-							break;
-						case BluetoothChatService.STATE_LISTEN:
-							break;
-						case BluetoothChatService.STATE_NONE:
-//                            setStatus(R.string.title_not_connected);
-							break;
-						case BluetoothChatService.STATE_LOST:
-							mConnectedDeviceName = null;
-							setStatusBarColour(R.color.status_noconnected);
-							setActionBarColour(getResources().getString(R.string.not_connected), R.color.title_noconnected);
-							break;
-					}
-					break;
-//                case Constants.MESSAGE_WRITE:
-//                    byte[] writeBuf = (byte[]) msg.obj;
-//                    // construct a string from the buffer
-//                    String writeMessage = new String(writeBuf);
-//                    mConversationArrayAdapter.add("Me:  " + writeMessage);
-//                    break;
-				case Constants.MESSAGE_READ:    //get the message
-					byte[] readBuf = (byte[]) msg.obj;
-					// construct a string from the valid bytes in the buffer
-					displayMessage(new String(readBuf, 0, msg.arg1));
-//                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
-					break;
-				case Constants.MESSAGE_DEVICE_NAME:
-					// save the connected device's name
-					mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
-					if (null != activity) {
-						Toast.makeText(activity, "Connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
-						setStatusBarColour(R.color.title_connected);
-						setActionBarColour(getResources().getString(R.string.title_connected_to) + " " + mConnectedDeviceName, R.color.status_connected);
-					}
-					break;
-				case Constants.MESSAGE_TOAST:
-					if (null != activity) {
-						Toast.makeText(activity, msg.getData().getString(Constants.TOAST), Toast.LENGTH_SHORT).show();
-					}
-					break;
-			}
-		}
-	};
-	/**
-	 * Member object for the chat services
-	 */
-	private BluetoothChatService mChatService = null;
-	/**
-	 * Local Bluetooth adapter
-	 */
-	private BluetoothAdapter mBluetoothAdapter = null;
-
-	private void displayMessage(String s) {
-		Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-	}
-
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -120,47 +36,6 @@ public class ReceiverActivity extends ColourBarActivity {
 	}
 
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		// Get local Bluetooth adapter
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		// If BT is not on, request that it be enabled.
-		// setupChat() will then be called during onActivityResult
-		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-			// Otherwise, setup the chat session
-		} else if (mChatService == null) {
-			// Initialize the BluetoothChatService to perform bluetooth connections
-			mChatService = new BluetoothChatService(this, mHandler);
-		}
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if (mChatService != null) {
-			mChatService.stop();
-		}
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		// Performing this check in onResume() covers the case in which BT was
-		// not enabled during onStart(), so we were paused to enable it...
-		// onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
-		if (mChatService != null) {
-			// Only if the state is STATE_NONE, do we know that we haven't started already
-			if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
-				// Start the Bluetooth chat services
-				mChatService.start();
-
-			}
-		}
-	}
 
 //    private void displayMessage(String message) {
 //        display.setText(message);
@@ -170,16 +45,6 @@ public class ReceiverActivity extends ColourBarActivity {
 		return ReceiverActivity.this;
 	}
 
-	/**
-	 * Makes this device discoverable.
-	 */
-	private void ensureDiscoverable() {
-		if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-			Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-			discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-			startActivity(discoverableIntent);
-		}
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -244,39 +109,6 @@ public class ReceiverActivity extends ColourBarActivity {
 		this.finish();
 	}
 
-	/**
-	 * Sends a message.
-	 *
-	 * @param message A string of text to send.
-	 */
-	private void sendMessage(String message) {
-		// Check that we're actually connected before trying anything
-		if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
-			Toast.makeText(getActivity(), R.string.not_connected, Toast.LENGTH_SHORT).show();
-			return;
-		}
-
-		// Check that there's actually something to send
-		if (message.length() > 0) {
-			// Get the message bytes and tell the BluetoothChatService to write
-			byte[] send = message.getBytes();
-			mChatService.write(send);
-
-		}
-	}
-
-	/**
-	 * Establish connection with other device
-	 */
-	private void connectDevice(String address) {
-
-		Log.d(TAG, address);
-		// Get the BluetoothDevice object
-		Log.d(TAG, "getRemoteDevice");
-		BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
-		// Attempt to connect to the device
-		mChatService.connect(device, false);
-	}
 
 	public void replaceFragment(Fragment fragment) {
 		Log.d(TAG, "Start new activity");
@@ -289,5 +121,10 @@ public class ReceiverActivity extends ColourBarActivity {
 
 	public void sentdata(String data) {
 		sendMessage(data);
+	}
+
+	@Override
+	public void onDataReceive(int status, int level) {
+
 	}
 }
--- app/src/main/java/com/starboy/karav/SA/SenderActivity.java
+++ app/src/main/java/com/starboy/karav/SA/SenderActivity.java
@@ -6,82 +6,20 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class SenderActivity extends ColourBarActivity {
+public class SenderActivity extends BluetoothActivity {
 
-	private static final int REQUEST_ENABLE_BT = 3;
-	/**
-	 * Name of the connected device
-	 */
-	private String mConnectedDeviceName = null;
-	/**
-	 * Member object for the chat services
-	 */
-	private BluetoothChatService mChatService = null;
-	/**
-	 * Local Bluetooth adapter
-	 */
-	private BluetoothAdapter mBluetoothAdapter = null;
 
-	/**
-	 * String buffer for outgoing messages
-	 */
-	private StringBuffer mOutStringBuffer;
 
 	private TextView display;
 	/**
 	 * The Handler that gets information back from the BluetoothChatService
 	 */
-	private final Handler mHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			Activity activity = getActivity();
-			switch (msg.what) {
-				case Constants.MESSAGE_STATE_CHANGE:
-					switch (msg.arg1) {
-						case BluetoothChatService.STATE_CONNECTED:
-//                            (getActivity()).setTitle(getString(R.string.title_connected_to, mConnectedDeviceName));
-//                            mConversationArrayAdapter.clear();
-							break;
-						case BluetoothChatService.STATE_CONNECTING:
-//                            setStatus(R.string.title_connecting);
-							break;
-						case BluetoothChatService.STATE_LISTEN:
-							break;
-						case BluetoothChatService.STATE_NONE:
-//                            setStatus(R.string.title_not_connected);
-							break;
-					}
-					break;
 
-				case Constants.MESSAGE_READ:    //get the message
-					byte[] readBuf = (byte[]) msg.obj;
-					// construct a string from the valid bytes in the buffer
-					displayMessage(new String(readBuf, 0, msg.arg1));
-//                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
-					break;
-				case Constants.MESSAGE_DEVICE_NAME:
-					// save the connected device's name
-					mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
-					if (null != activity) {
-						Toast.makeText(activity, "Connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
-					}
-					break;
-				case Constants.MESSAGE_TOAST:
-					if (null != activity) {
-						Toast.makeText(activity, msg.getData().getString(Constants.TOAST), Toast.LENGTH_SHORT).show();
-					}
-					break;
-			}
-		}
-	};
 	private Button discover;
 	private Button sb;
 	private Button su;
@@ -183,61 +121,8 @@ public class SenderActivity extends ColourBarActivity {
 
 	}
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		// Get local Bluetooth adapter
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		// If BT is not on, request that it be enabled.
-		// setupChat() will then be called during onActivityResult
-		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-			// Otherwise, setup the chat session
-		} else if (mChatService == null) {
-			// Initialize the BluetoothChatService to perform bluetooth connections
-			mChatService = new BluetoothChatService(getApplicationContext(), mHandler);
-		}
-		// Initialize the buffer for outgoing messages
-		mOutStringBuffer = new StringBuffer("");
-	}
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if (mChatService != null) {
-			mChatService.stop();
-		}
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		// Performing this check in onResume() covers the case in which BT was
-		// not enabled during onStart(), so we were paused to enable it...
-		// onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
-		if (mChatService != null) {
-			// Only if the state is STATE_NONE, do we know that we haven't started already
-			if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
-				// Start the Bluetooth chat services
-				mChatService.start();
-			}
-		}
-	}
-
-	/**
-	 * Makes this device discoverable.
-	 */
-	private void ensureDiscoverable() {
-		if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-			Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-			discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-			startActivity(discoverableIntent);
-		}
-	}
-
-	private void displayMessage(String message) {
+	private void displayMessagerecieve(String message) {
 		display.setText(message);
 	}
 
@@ -258,27 +143,5 @@ public class SenderActivity extends ColourBarActivity {
 		return SenderActivity.this;
 	}
 
-	/**
-	 * Sends a message.
-	 *
-	 * @param message A string of text to send.
-	 */
-	private void sendMessage(String message) {
-		// Check that we're actually connected before trying anything
-		if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
-			Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
-			return;
-		}
 
-		// Check that there's actually something to send
-		if (message.length() > 0) {
-			// Get the message bytes and tell the BluetoothChatService to write
-			byte[] send = message.getBytes();
-			mChatService.write(send);
-
-//			// Reset out string buffer to zero and clear the edit text field
-			mOutStringBuffer.setLength(0);
-//			mOutEditText.setText(mOutStringBuffer);
-		}
-	}
 }
