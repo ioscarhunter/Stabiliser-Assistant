49434fecba5ca8276b3561bdebf5d78cd76f3047
Makemek <makemek101@hotmail.com>
2015-05-19 15:57:01 +0700

    Initial logRecorder module commit

---
+++ logrecorder/src/main/java/com/example/make/logrecorder/LogRec.java
@@ -0,0 +1,16 @@
+package com.example.make.logrecorder;
+
+/**
+ * Created by Apipol on 19/05/15.
+ */
+public class LogRec {
+
+    public void saveResults(int level, int balance, int totalBalance) {
+        throw new UnsupportedOperationException();
+    }
+
+    public TestResult[] getAllResult() {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ logrecorder/src/main/java/com/example/make/logrecorder/TestResult.java
@@ -0,0 +1,14 @@
+package com.example.make.logrecorder;
+
+/**
+ * Created by Apipol on 19/05/15.
+ * A record of result for each completed test contains
+ * - id
+ * - level (A, B, C, D, E)
+ * - timestamp (mm:ss)
+ * - #balance number balance pass
+ * - total number of sampling
+ */
+public class TestResult {
+
+}
